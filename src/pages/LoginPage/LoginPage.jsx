35f4b322610577e5fd6275ec089361acf6614be9
Heather Etheridge <heathernetheridge@gmail.com>
2019-10-06 21:30:25 -0500

    Log in functionality now working

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -38,12 +38,26 @@ class LoginPage extends Component {
         <form className="form-horizontal" onSubmit={this.handleSubmit} >
           <div className="form-group">
             <div className="col-sm-12">
-              <input type="email" className="form-control" placeholder="Email" value={this.state.email} name="email" onChange={this.handleChange} />
+              <input 
+                type="email" 
+                className="form-control" 
+                placeholder="Email" 
+                value={this.state.email} 
+                name="email" 
+                onChange={this.handleChange} 
+              />
             </div>
           </div>
           <div className="form-group">
             <div className="col-sm-12">
-              <input type="password" className="form-control" placeholder="Password" value={this.state.pw} name="pw" onChange={this.handleChange} />
+              <input 
+                type="password" 
+                className="form-control" 
+                placeholder="Password" 
+                value={this.state.pw} 
+                name="pw" 
+                onChange={this.handleChange} 
+              />
             </div>
           </div>
           <div className="form-group">
